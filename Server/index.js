17dd6aaccdc43326637a9612ad8f084b2ed0c536
David <dlongaron@hotmail.com>
2021-11-10 15:16:11 +0100

    Linting set, maybe

---
+++ Server/index.js
@@ -0,0 +1,22 @@
+const PORT =3001;
+const express= require('express');
+const cors= require('cors');
+const app= express();
+const router= require('./router');
+const db=require('./models');
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+(async function () {
+  try {
+    await db;
+    app.listen(PORT,()=>{
+      console.log('Server is running on Port 3001');
+    });
+  } catch (error) {
+    console.log(error);
+
+  }
+})();
